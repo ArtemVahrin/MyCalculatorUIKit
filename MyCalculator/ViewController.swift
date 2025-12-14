323544141efb78e8d01b5336cd158b1e5ff24676
ArtemVahrin <artemvahrin6@gmail.com>
2025-12-14 22:54:50 +0300

    Refactor creating buttons alghoritm

--- MyCalculator/ViewController.swift
+++ MyCalculator/ViewController.swift
@@ -22,163 +22,46 @@ class ViewController: UIViewController {
         $0.spacing = 10
     return $0
     }(UIStackView())
-    
-    let numButton0: UIButton = {
-        $0.setTitle("0", for: .normal)
-//        $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let numButton1: UIButton = {
-        $0.setTitle("1", for: .normal)
-  //      $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let numButton2: UIButton = {
-        $0.setTitle("2", for: .normal)
-    //    $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-        
-    }(UIButton())
-    
-    let numButton3: UIButton = {
-        $0.setTitle("3", for: .normal)
-      //  $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let numButton4: UIButton = {
-        $0.setTitle("4", for: .normal)
-        //$0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let numButton5: UIButton = {
-        $0.setTitle("5", for: .normal)
-        //$0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    let numButton6: UIButton = {
-        $0.setTitle("6", for: .normal)
- //       $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    let numButton7: UIButton = {
-        $0.setTitle("7", for: .normal)
-//        $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    let numButton8: UIButton = {
-        $0.setTitle("8", for: .normal)
-//        $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    let numButton9: UIButton = {
-        $0.setTitle("9", for: .normal)
-//        $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let plusButton: UIButton = {
-        $0.setTitle("+", for: .normal)
-//        $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let minusButton: UIButton = {
-        $0.setTitle("-", for: .normal)
-//        $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let multiplyButton: UIButton = {
-        $0.setTitle("*", for: .normal)
-        return $0
-    }(UIButton())
-    
-    let divideButton: UIButton = {
-        $0.setTitle("/", for: .normal)
-//        $0.addTarget(ViewController.self, action: #selector(printButtonTapped), for: .touchUpInside)
-        return $0
-    }(UIButton())
-    
-    let deleteLastButton: UIButton = {
-        $0.setTitle("del", for: .normal)
-        return $0
-    }(UIButton())
-    
-    let clearAllButton: UIButton = {
-        $0.setTitle("AC", for: .normal)
-        return $0
-    }(UIButton())
-    
-    let percentButton: UIButton = {
-        $0.setTitle("%", for: .normal)
-        return $0
-    }(UIButton())
 
-    let outputButton: UIButton = {
-        $0.setTitle("=", for: .normal)
-        return $0
-    }(UIButton())
-    
-    let changeSignButton: UIButton = {
-        $0.setTitle("+/-", for: .normal)
-        return $0
-    }(UIButton())
-    
-    let commaButton: UIButton = {
-        $0.setTitle(",", for: .normal)
-        return $0
-    }(UIButton())
-    
+    let allLabelsByLinesArray: [[String]] = [["+/-","0",",", "="], ["1", "2", "3", "+"],[ "4", "5", "6", "-"], ["7", "8", "9", "*"],[ "del", "AC", "%", "/" ]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
         view.addSubview(verticalStack1)
         view.addSubview(inputAndOutputLabel)
+        var allButtonsArrays = [[UIButton]]()
+        var line = [UIButton]()
         
-        let bottomButtons: [UIButton] = [changeSignButton, numButton0, commaButton, outputButton]
-        let stack123Buttons: [UIButton] = [numButton1, numButton2, numButton3, plusButton]
-        
-        let stack456Buttons: [UIButton] = [numButton4, numButton5, numButton6, minusButton]
-        
-        let stack789Buttons: [UIButton] = [numButton7, numButton8, numButton9, multiplyButton]
-        
-        let topButtons: [UIButton] = [deleteLastButton, clearAllButton, percentButton, divideButton]
-        
-        let signButtons: [UIButton] = [percentButton, divideButton, multiplyButton, minusButton, plusButton, commaButton, changeSignButton]
-        
-        let numsButtons: [UIButton] = [numButton0, numButton1, numButton2, numButton3, numButton4, numButton5, numButton6, numButton7, numButton8, numButton9]
-        
-        let allButtons = stack123Buttons + stack456Buttons + stack789Buttons + topButtons + bottomButtons
-        
-        let stack1 = createStack(from: stack123Buttons)
-        let stack2 = createStack(from: stack456Buttons)
-        let stack3 = createStack(from: stack789Buttons)
-        let stack0 = createStack(from: bottomButtons)
-        let stack4 = createStack(from: topButtons)
-        
-        verticalStack1.addArrangedSubview(stack4)
-        verticalStack1.addArrangedSubview(stack3)
-        verticalStack1.addArrangedSubview(stack2)
-        verticalStack1.addArrangedSubview(stack1)
-        verticalStack1.addArrangedSubview(stack0)
-
-        
-        for button in allButtons {
-            if numsButtons.contains(button) {
-                button.backgroundColor = .lightGray
+        for labelLine in allLabelsByLinesArray {
+            line = []
+            for buttonLabel in labelLine {
+                line.append(createButton(with: buttonLabel))
             }
-            else {
-                button.backgroundColor = .red
-            }
-            button.addTarget(self, action: #selector(printButtonTapped), for: .touchUpInside)
-            button.layer.cornerRadius = 8
-
+            allButtonsArrays.append(line)
+            
         }
+        
+        for buttonLine in allButtonsArrays {
+            let stack = createStack(from: buttonLine)
+            verticalStack1.addArrangedSubview(stack)
+        }
+        
+//        for button in allButtons {
+//            if numsButtons.contains(button) {
+//                button.backgroundColor = .lightGray
+//            } else if button.currentTitle == "=" {
+//                button.addTarget(self, action: #selector(mathOperation), for: .touchUpInside)
+//            }
+//            else {
+//                button.backgroundColor = .red
+//            }
+//            button.addTarget(self, action: #selector(printButtonTapped), for: .touchUpInside)
+//            button.layer.cornerRadius = 8
+//
+//        }
+//        
+        
         setConstraints()
     }
     
@@ -197,6 +80,9 @@ class ViewController: UIViewController {
         return horizontalStack
     }
     
+    func createAllButtons() {
+        
+    }
     @objc func printButtonTapped(_ button: UIButton) {
         if button.currentTitle == "del"{
             if inputAndOutputLabel.text != "" {
@@ -211,10 +97,18 @@ class ViewController: UIViewController {
         }
     }
     
-    func mathOperation(_ str: String) -> String {
+    
+    @objc func mathOperation(_ str: String) -> String {
         var ans = ""
         
         return ans
+    }
+    
+    func createButton(with sign: String) -> UIButton {
+        let button = UIButton()
+        button.backgroundColor = .gray
+        button.setTitle(sign, for: .normal)
+        return button
     }
 
     private func setConstraints() {
